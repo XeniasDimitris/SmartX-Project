07635a0b4aa8e1bf250ec805887b0d3e6d3a03a0
XeniasDimitris <dimitrisxenias@gmail.com>
2020-11-12 15:42:16 +0200

    Weather Api is completed

--- back-end/api/utils.py
+++ back-end/api/utils.py
@@ -3,7 +3,9 @@ import pandas as pd
 import gc
 
 
-def get_weather_data(file, typeof, start, end):
+def get_weather_data(file, typeof, request):
+
+    # As we have one json for each day, we concatenate them into an appropriate format for DF constructor
     data = {'datetime': [], 'value': []}
     with open(file, 'r') as f:
         for line in f:
@@ -17,11 +19,20 @@ def get_weather_data(file, typeof, start, end):
                 else:
                     data['datetime'].append(key)
                     data['value'].append(typeof(dictionary[key]))
+
+    # Creation of Dataframe
     df = pd.DataFrame(data=data)
     df['datetime_pd'] = pd.to_datetime(df['datetime'], infer_datetime_format=True)
     df = df.set_index(['datetime_pd'])
+
+    start = request.query_params['start'] if 'start' in request.query_params else None
+    end = request.query_params['end'] if 'end' in request.query_params else None
+
+    # Query with datetime index
     query = df.loc[start:end, ['datetime', 'value']].sort_index()
     res = query.to_dict('records')
+
+    # Free up memory
     del df
     gc.collect()
     return res
--- back-end/api/views/weather_views.py
+++ back-end/api/views/weather_views.py
@@ -1,71 +1,49 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from datetime import datetime
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from ..helpers import get_weather_data
-import pandas as pd
-# Create your views here.
+from ..utils import get_weather_data
+
 data_dir = '/home/dimitris/Desktop/DiplomaThesis/Datasets/Aarhus'
 
 
 class WeatherHumidity(APIView):
 
     def get(self, request, format=None):
-        print(request.query_params)
-        start = request.query_params['start'] if 'start' in request.query_params else None
-        end = request.query_params['end'] if 'end' in request.query_params else None
-        res = get_weather_data(f'{data_dir}/Weather/hum.txt', int, start, end)
+        res = get_weather_data(f'{data_dir}/Weather/hum.txt', int, request)
         return Response(res, status=status.HTTP_200_OK)
 
 
 class WeatherTemperature(APIView):
 
     def get(self, request, format=None):
-        print(request.query_params)
-        start = request.query_params['start'] if 'start' in request.query_params else None
-        end = request.query_params['end'] if 'end' in request.query_params else None
-        res = get_weather_data(f'{data_dir}/Weather/tempm.txt', int, start, end)
+        res = get_weather_data(f'{data_dir}/Weather/tempm.txt', int, request)
         return Response(res, status=status.HTTP_200_OK)
-
 
 
 class WeatherPressure(APIView):
 
     def get(self, request, format=None):
-        print(request.query_params)
-        start = request.query_params['start'] if 'start' in request.query_params else None
-        end = request.query_params['end'] if 'end' in request.query_params else None
-        res = get_weather_data(f'{data_dir}/Weather/pressurem.txt', int, start, end)
+        res = get_weather_data(f'{data_dir}/Weather/pressurem.txt', int, request)
         return Response(res, status=status.HTTP_200_OK)
 
 
 class WeatherWindSpeed(APIView):
 
     def get(self, request, format=None):
-        print(request.query_params)
-        start = request.query_params['start'] if 'start' in request.query_params else None
-        end = request.query_params['end'] if 'end' in request.query_params else None
-        res = get_weather_data(f'{data_dir}/Weather/wspdm.txt', int, start, end)
+        res = get_weather_data(f'{data_dir}/Weather/wspdm.txt', int, request)
         return Response(res, status=status.HTTP_200_OK)
 
 
 class WeatherWindDirection(APIView):
 
     def get(self, request, format=None):
-        print(request.query_params)
-        start = request.query_params['start'] if 'start' in request.query_params else None
-        end = request.query_params['end'] if 'end' in request.query_params else None
-        res = get_weather_data(f'{data_dir}/Weather/wdird.txt', int, start, end)
+        res = get_weather_data(f'{data_dir}/Weather/wdird.txt', int, request)
         return Response(res, status=status.HTTP_200_OK)
 
 
 class WeatherDew(APIView):
 
     def get(self, request, format=None):
-        print(request.query_params)
-        start = request.query_params['start'] if 'start' in request.query_params else None
-        end = request.query_params['end'] if 'end' in request.query_params else None
-        res = get_weather_data(f'{data_dir}/Weather/dewptm.txt', int, start, end)
+        res = get_weather_data(f'{data_dir}/Weather/dewptm.txt', int, request)
         return Response(res, status=status.HTTP_200_OK)
